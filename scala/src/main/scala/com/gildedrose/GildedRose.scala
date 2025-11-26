86deced94e1a96c10e4525cfba7bf84453983ae3
Nikola Milutinovic <nikola.milutinovic@cenosco.com>
2025-11-26 21:24:13 +0100

    [3] Move item update to Item.

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -11,53 +11,7 @@ class GildedRose(val items: Array[Item]) {
 
   def updateQuality() : Unit = {
     for (item <- items) {
-      if (!item.name.equals(ItemName.BRIE) && !item.name.equals(ItemName.BACKSTAGE)) {
-        if (item.quality > 0) {
-          if (!item.name.equals(ItemName.SULFURAS)) {
-            item.quality = item.quality - 1
-          }
-        }
-      } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1
-
-          if (item.name.equals(ItemName.BACKSTAGE)) {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1
-              }
-            }
-
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1
-              }
-            }
-          }
-        }
-      }
-
-      if (!item.name.equals(ItemName.SULFURAS)) {
-        item.sellIn = item.sellIn - 1
-      }
-
-      if (item.sellIn < 0) {
-        if (!item.name.equals(ItemName.BRIE)) {
-          if (!item.name.equals(ItemName.BACKSTAGE)) {
-            if (item.quality > 0) {
-              if (!item.name.equals(ItemName.SULFURAS)) {
-                item.quality = item.quality - 1
-              }
-            }
-          } else {
-            item.quality = item.quality - item.quality
-          }
-        } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1
-          }
-        }
-      }
+      item.updateItem()
     }
   }
 }
--- scala/src/main/scala/com/gildedrose/Item.scala
+++ scala/src/main/scala/com/gildedrose/Item.scala
@@ -1,5 +1,53 @@
 package com.gildedrose
 
 case class Item(name: String, var sellIn: Int, var quality: Int) {
+  def updateItem(): Unit = {
+    if (!name.equals(ItemName.BRIE) && !name.equals(ItemName.BACKSTAGE)) {
+      if (quality > 0) {
+        if (!name.equals(ItemName.SULFURAS)) {
+          quality = quality - 1
+        }
+      }
+    } else {
+      if (quality < 50) {
+        quality = quality + 1
 
+        if (name.equals(ItemName.BACKSTAGE)) {
+          if (sellIn < 11) {
+            if (quality < 50) {
+              quality = quality + 1
+            }
+          }
+
+          if (sellIn < 6) {
+            if (quality < 50) {
+              quality = quality + 1
+            }
+          }
+        }
+      }
+    }
+
+    if (!name.equals(ItemName.SULFURAS)) {
+      sellIn = sellIn - 1
+    }
+
+    if (sellIn < 0) {
+      if (!name.equals(ItemName.BRIE)) {
+        if (!name.equals(ItemName.BACKSTAGE)) {
+          if (quality > 0) {
+            if (!name.equals(ItemName.SULFURAS)) {
+              quality = quality - 1
+            }
+          }
+        } else {
+          quality = quality - quality
+        }
+      } else {
+        if (quality < 50) {
+          quality = quality + 1
+        }
+      }
+    }
+  }
 }
