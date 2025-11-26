8d755a5512bc220260f905ee7f46122382e912e9
Nikola Milutinovic <nikola.milutinovic@cenosco.com>
2025-11-26 20:49:51 +0100

    [3] Factor out name constants.

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -1,14 +1,20 @@
 package com.gildedrose
 
+object ItemName {
+  val BRIE = "Aged Brie"
+  val BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert"
+  val SULFURAS = "Sulfuras, Hand of Ragnaros"
+}
+
 class GildedRose(val items: Array[Item]) {
 
 
   def updateQuality() : Unit = {
     for (item <- items) {
-      if (!item.name.equals("Aged Brie")
-        && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+      if (!item.name.equals(ItemName.BRIE)
+        && !item.name.equals(ItemName.BACKSTAGE)) {
         if (item.quality > 0) {
-          if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+          if (!item.name.equals(ItemName.SULFURAS)) {
             item.quality = item.quality - 1
           }
         }
@@ -16,7 +22,7 @@ class GildedRose(val items: Array[Item]) {
         if (item.quality < 50) {
           item.quality = item.quality + 1
 
-          if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+          if (item.name.equals(ItemName.BACKSTAGE)) {
             if (item.sellIn < 11) {
               if (item.quality < 50) {
                 item.quality = item.quality + 1
@@ -32,15 +38,15 @@ class GildedRose(val items: Array[Item]) {
         }
       }
 
-      if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+      if (!item.name.equals(ItemName.SULFURAS)) {
         item.sellIn = item.sellIn - 1
       }
 
       if (item.sellIn < 0) {
-        if (!item.name.equals("Aged Brie")) {
-          if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+        if (!item.name.equals(ItemName.BRIE)) {
+          if (!item.name.equals(ItemName.BACKSTAGE)) {
             if (item.quality > 0) {
-              if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+              if (!item.name.equals(ItemName.SULFURAS)) {
                 item.quality = item.quality - 1
               }
             }
