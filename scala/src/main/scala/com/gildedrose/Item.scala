9375426213285e27d07e02d3e564d73c7d81820b
Nikola Milutinovic <nikola.milutinovic@cenosco.com>
2025-11-26 22:19:40 +0100

    [3] Cleanup if-conditionals

--- scala/src/main/scala/com/gildedrose/Item.scala
+++ scala/src/main/scala/com/gildedrose/Item.scala
@@ -3,83 +3,59 @@ package com.gildedrose
 case class Item(name: String, var sellIn: Int, var quality: Int) {
   def updateItem(): Unit = {
     val isAgedBrie = name.equals(ItemName.BRIE)
+    val isBackstagePass = name.equals(ItemName.BACKSTAGE)
 
     if (isAgedBrie) {
-      {
+      if (quality < 50) {
+        quality = quality + 1
+      }
+      sellIn = sellIn - 1
+
+      if (sellIn < 0) {
         if (quality < 50) {
           quality = quality + 1
-
-          if (name.equals(ItemName.BACKSTAGE)) {
-            if (sellIn < 11) {
-              if (quality < 50) {
-                quality = quality + 1
-              }
-            }
-
-            if (sellIn < 6) {
-              if (quality < 50) {
-                quality = quality + 1
-              }
-            }
-          }
-        }
-
-        if (!name.equals(ItemName.SULFURAS)) {
-          sellIn = sellIn - 1
-        }
-
-        if (sellIn < 0) {
-          if (quality < 50) {
-            quality = quality + 1
-          }
         }
       }
     } else {
-      {
-        if (!name.equals(ItemName.BACKSTAGE)) {
+      if (!isBackstagePass) {
+        if (quality > 0) {
+          if (!name.equals(ItemName.SULFURAS)) {
+            quality = quality - 1
+          }
+        }
+      } else {
+        if (quality < 50) {
+          quality = quality + 1
+
+          if (sellIn < 11) {
+            if (quality < 50) {
+              quality = quality + 1
+            }
+          }
+
+          if (sellIn < 6) {
+            if (quality < 50) {
+              quality = quality + 1
+            }
+          }
+        }
+      }
+
+      if (!name.equals(ItemName.SULFURAS)) {
+        sellIn = sellIn - 1
+      }
+
+      if (sellIn < 0) {
+        if (!isBackstagePass) {
           if (quality > 0) {
             if (!name.equals(ItemName.SULFURAS)) {
               quality = quality - 1
             }
           }
         } else {
-          if (quality < 50) {
-            quality = quality + 1
-
-            if (name.equals(ItemName.BACKSTAGE)) {
-              if (sellIn < 11) {
-                if (quality < 50) {
-                  quality = quality + 1
-                }
-              }
-
-              if (sellIn < 6) {
-                if (quality < 50) {
-                  quality = quality + 1
-                }
-              }
-            }
-          }
-        }
-
-        if (!name.equals(ItemName.SULFURAS)) {
-          sellIn = sellIn - 1
-        }
-
-        if (sellIn < 0) {
-          if (!name.equals(ItemName.BACKSTAGE)) {
-            if (quality > 0) {
-              if (!name.equals(ItemName.SULFURAS)) {
-                quality = quality - 1
-              }
-            }
-          } else {
-            quality = quality - quality
-          }
+          quality = quality - quality
         }
       }
     }
-
   }
-
 }
