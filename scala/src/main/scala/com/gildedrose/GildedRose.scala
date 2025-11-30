e576c9300bce9de6c7364c32ca4ae922cd1eedaa
Nikola Milutinovic <nikola.milutinovic@cenosco.com>
2025-11-30 19:33:35 +0100

    [4] Hook up converter registry with main entry point.

--- scala/src/main/scala/com/gildedrose/GildedRose.scala
+++ scala/src/main/scala/com/gildedrose/GildedRose.scala
@@ -1,13 +1,13 @@
 package com.gildedrose
 
+import registry.ConverterRegistry
+
 class GildedRose(val items: Array[Item]) {
 
 
   def updateQuality() : Unit = {
-    val processor = ItemProcessor()
     for (item <- items) {
-      //processor.updateItem(item)
-      val internal = InternalItem(item)
+      val internal = ConverterRegistry.default.convert(item)
       val updated = internal.update()
       item.quality = updated.quality
       item.sellIn = updated.sellIn
